b38d1451a3343bd1aeb1db1c20a87384f20ed027
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-11-23 17:00:45 +0800

    Add orderby thenby test case for practice

---
+++ CSharpAdvanceDesignTests/KyoOrderByThenBy.cs
@@ -0,0 +1,67 @@
+﻿using ExpectedObjects;
+using Lab.Entities;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    public class KyoOrderByThenBy
+    {
+        [Test]
+        public void orderBy_lastName_and_firstName()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyOrderByLastNameAndFirstName(employees);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName(IEnumerable<Employee> employees)
+        {
+            //bubble sort
+            var stringComparer = Comparer<string>.Default;
+            var elements = employees.ToList();
+            while (elements.Any())
+            {
+                var minElement = elements[0];
+                var index = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    if (stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
+                    {
+                        minElement = elements[i];
+                        index = i;
+                    }
+                    else if (stringComparer.Compare(elements[i].LastName, minElement.LastName) == 0)
+                    {
+                        if (stringComparer.Compare(elements[i].FirstName, minElement.FirstName) < 0)
+                        {
+                            minElement = elements[i];
+                            index = i;
+                        }
+                    }
+                }
+
+                elements.RemoveAt(index);
+                yield return minElement;
+            }
+        }
+    }
+}
